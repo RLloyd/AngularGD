f98d8f288ff7206ab7a02a5c9ed18f10544edb77
RLoyd <rolandolloyd@gmail.com>
2015-03-24 22:43:18 -0400

    R2b
    Testing

---
+++ js/registrationCtrl.js
@@ -0,0 +1,51 @@
+/*************| LOGIN/REGISTRATION CONTROLLER |*************/
+
+(function () {
+
+    var app = angular.module("regContlr", []);
+    app.controller("RegistrationController", function ($scope, $firebaseSimpleLogin, $location) {
+
+        var fireRefSL = new Firebase("https://angulargdapp.firebaseio.com/");
+        var simpleLogin = $firebaseSimpleLogin(fireRefSL);
+        
+        /*-----------|login() function. This will route to the location.path of /meetings in app.js where it's been registered*/
+        $scope.login = function () {
+            simpleLogin.$login("password", {
+                email : $scope.user.email,
+                password: $scope.user.password
+            })
+            .then(function(user){
+                $location.path("/meetings");
+            }, 
+            function(error){
+                $scope.message = error.toString();
+            });
+            /*$location.path("/meetings");*/
+            /*console.log("$location.path: " + $location.path);*/
+            /*-----------|This function will be called by ng-submit in login.html*/
+            /*alert($scope.user.email);*/
+        };
+        
+        /*-----------|register() function.*/
+        $scope.register = function () {
+            $location.path("/meetings");
+        };
+
+        /*-----------|$scope snoops around. Feed the xprsion {{name}} and can get info back with $scope.$on(...) */
+        /*$scope.name = "Odee";
+        $scope.$on("$viewContentLoaded", function () {
+            console.log("myForm loaded!: " + $scope.myForm);
+        });
+        console.log("myForm not loaded yet!: " + $scope.myForm);
+        console.log("RegistrationController Loaded!" + app);
+*/
+        /*-----------|This function will be called by ng-submit in login.html*/
+        /*$scope.login = function(){
+            alert($scope.user.email);
+        }*/
+
+
+
+    });
+
+}());
